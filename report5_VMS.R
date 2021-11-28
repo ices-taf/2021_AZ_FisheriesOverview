4eca1eb8a4ee27da991a13d01e384f8997714ea8
Colin Millar <colin.millar@ices.dk>
2021-11-29 00:03:32 +0100

    Update report5_VMS.R

--- report5_VMS.R
+++ report5_VMS.R
@@ -25,10 +25,10 @@ effort <-
 effort <- dplyr::select(effort, -WKT)
 
 # read vms swept area ratio
-sar <-
-  sf::st_read("vms_sar.csv",
-               options = "GEOM_POSSIBLE_NAMES=wkt", crs = 4326)
-sar <- dplyr::select(sar, -WKT)
+#sar <-
+#  sf::st_read("vms_sar.csv",
+#               options = "GEOM_POSSIBLE_NAMES=wkt", crs = 4326)
+#sar <- dplyr::select(sar, -WKT)
 
 
 #set range of years in plots
@@ -81,15 +81,15 @@ ggplot2::ggsave(paste0(cap_year, "_", ecoreg_code,"_FO_VMS_effort.png"), path = 
 #~~~~~~~~~~~~~~~#
 
 # write layer
-write_layer(sar, paste0(year_cap, "_", ecoreg,"_FO_VMS_sar"))
+#write_layer(sar, paste0(year_cap, "_", ecoreg,"_FO_VMS_sar"))
 
-plot_sar_map(sar, ecoregion, what = "surface") +
-  ggtitle(paste0("Average surface swept area ratio ",year_range))
+#plot_sar_map(sar, ecoregion, what = "surface") +
+#  ggtitle(paste0("Average surface swept area ratio ",year_range))
 
-ggplot2::ggsave(paste0(year_cap, "_", ecoreg,"_FO_VMS_sarA.png"), path = "report", width = 170, height = 200, units = "mm", dpi = 300)
+#ggplot2::ggsave(paste0(year_cap, "_", ecoreg,"_FO_VMS_sarA.png"), path = "report", width = 170, height = 200, units = "mm", dpi = 300)
 
-plot_sar_map(sar, ecoregion, what = "subsurface")+
-  ggtitle(paste0("Average subsurface swept area ratio ",year_range))
+#plot_sar_map(sar, ecoregion, what = "subsurface")+
+#  ggtitle(paste0("Average subsurface swept area ratio ",year_range))
 
-ggplot2::ggsave(paste0(year_cap, "_", ecoreg,"_FO_VMS_sarB.png"), path = "report", width = 170, height = 200, units = "mm", dpi = 300)
+#ggplot2::ggsave(paste0(year_cap, "_", ecoreg,"_FO_VMS_sarB.png"), path = "report", width = 170, height = 200, units = "mm", dpi = 300)
 
